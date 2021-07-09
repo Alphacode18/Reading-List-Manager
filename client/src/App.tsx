eac18b0fbabc8c948e39f27a5062624aae77af00
Alphacode18 <skharban@purdue.edu>
2021-07-09 07:55:20 +0530

    Front End Components Started

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div id='main'>
+      <h1>Shreyas' Reading List</h1>
+      <BookList />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/BookList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div id='main'>
+      <ul id='book-list'>
+        <li>The Alamanac Of Naval Ravikant</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
