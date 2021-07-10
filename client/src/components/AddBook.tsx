a7b1b6fd06cf994bb06c8a54f72a544cf8e62bfb
Alphacode18 <skharban@purdue.edu>
2021-07-10 08:04:48 +0530

    Stateful Components Added

--- client/src/components/AddBook.tsx
+++ client/src/components/AddBook.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
@@ -9,29 +9,56 @@ interface Author {
 }
 
 function AddBook(props: any) {
+  const [name, setName] = useState<HTMLInputElement | null>();
+  const [genre, setGenre] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (event: React.FormEvent): void => {
+    event.preventDefault();
+    console.log(name, genre, author);
+  };
+
   const displayAuthors = () => {
     const { authors, loading } = props.data;
     return loading ? (
       <option disabled>Loading Authors...</option>
     ) : (
       authors.map((author: Author) => {
-        return <option key={author.id}>{author.name}</option>;
+        return (
+          <option value={author.name} key={author.id}>
+            {author.name}
+          </option>
+        );
       })
     );
   };
   return (
-    <form id='add-book'>
+    <form id='add-book' onSubmit={handleSubmit}>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input
+          type='text'
+          onChange={(event: any) => {
+            setName(event.target.value);
+          }}
+        />
       </div>
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input
+          type='text'
+          onChange={(event: any) => {
+            setGenre(event.target.value);
+          }}
+        />
       </div>
       <div className='field'>
         <label>Author:</label>
-        <select>
+        <select
+          onChange={(event: any) => {
+            setAuthor(event.target.value);
+          }}
+        >
           <option>Select author</option>
           {displayAuthors()}
         </select>
