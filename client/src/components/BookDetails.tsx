4eff7f59d843e6347bdf3013128180a9b1739787
Alphacode18 <skharban@purdue.edu>
2021-07-11 12:30:38 +0530

    Styling Added

--- client/src/components/BookDetails.tsx
+++ client/src/components/BookDetails.tsx
@@ -14,24 +14,26 @@ interface BookDetailsProp {
 }
 
 function BookDetails(props: BookDetailsProp) {
-  console.log(props);
-  const { book } = props.data;
-  if (book) {
-    return (
-      <div id='book-details'>
-        <h2>{book.name} </h2>
-        <p>{book.genre}</p>
-        <p>{book.author.name}</p>
-        <p>All books by this author</p>
-        <ul className='other-books'>
-          {book.author.books.map((item: any) => {
-            return <li key={item.id}>{item.name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-  return <p>No Book Selected </p>;
+  const displayBookDetails = () => {
+    const { book } = props.data;
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name} </h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul className='other-books'>
+            {book.author.books.map((item: any) => {
+              return <li key={item.id}>{item.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+    return <div>No Book Selected...</div>;
+  };
+  return <div id='book-details'>{displayBookDetails()}</div>;
 }
 
 export default graphql(getBook, {
